4139946e31683c8c127ea12eb128a7b39d8859c7
Дмитро Ковтунович <127244832+Ke1fosao@users.noreply.github.com>
2026-07-21 23:32:39 +0300

    feat: register settings and reaction routers

--- app/bot/setup.py
+++ app/bot/setup.py
@@ -2,6 +2,8 @@ from aiogram import Dispatcher
 
 from app.bot.routers.groups import router as groups_router
 from app.bot.routers.private import router as private_router
+from app.bot.routers.reactions import router as reactions_router
+from app.bot.routers.settings import router as settings_router
 from app.repositories.activity import ActivityRepository
 
 
@@ -14,5 +16,7 @@ def build_dispatcher(
     dispatcher["repository"] = repository
     dispatcher["default_timezone"] = default_timezone
     dispatcher.include_router(private_router)
+    dispatcher.include_router(settings_router)
+    dispatcher.include_router(reactions_router)
     dispatcher.include_router(groups_router)
     return dispatcher
