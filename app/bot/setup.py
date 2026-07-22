14d61ad15d8abbe0261fa79d20d1eebfc25b0f02
Дмитро Ковтунович <127244832+Ke1fosao@users.noreply.github.com>
2026-07-22 17:55:26 +0300

    feat: use achievement v2 gamification repository

--- app/bot/setup.py
+++ app/bot/setup.py
@@ -7,7 +7,7 @@ from app.bot.routers.private import router as private_router
 from app.bot.routers.reactions import router as reactions_router
 from app.bot.routers.settings import router as settings_router
 from app.repositories.activity import ActivityRepository
-from app.repositories.gamification import GamificationRepository
+from app.repositories.gamification_v2 import AchievementGamificationRepository
 from app.repositories.miniapp import MiniAppRepository
 from app.repositories.owner import OwnerRepository
 
@@ -30,7 +30,9 @@ def build_dispatcher(
     dispatcher = Dispatcher()
     resolved_owner_repository = owner_repository or OwnerRepository(repository._session_factory)
     dispatcher["repository"] = repository
-    dispatcher["gamification_repository"] = GamificationRepository(repository._session_factory)
+    dispatcher["gamification_repository"] = AchievementGamificationRepository(
+        repository._session_factory
+    )
     dispatcher["miniapp_repository"] = MiniAppRepository(repository._session_factory)
     dispatcher["owner_repository"] = resolved_owner_repository
     dispatcher["default_timezone"] = default_timezone
