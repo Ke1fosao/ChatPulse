10a752038d1062ea0b3841a8be58f59ca3636024
Дмитро Ковтунович <127244832+Ke1fosao@users.noreply.github.com>
2026-07-22 11:24:46 +0300

    feat: load owner panel styles

--- miniapp/src/main.tsx
+++ miniapp/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { OwnerApp } from "./owner/OwnerApp";
 import "./styles/global.css";
+import "./styles/owner.css";
 
 const isOwnerRoute = window.location.pathname.replace(/\/+$/, "") === "/miniapp/owner";
 const RootApp = isOwnerRoute ? OwnerApp : App;
